9595a9e6d12228581e043687f93dcdd2d85b15da
sektant-gabe <65908248+sektant-gabe@users.noreply.github.com>
2025-08-22 17:44:01 -0300

    1.1.0

--- init.lua
+++ init.lua
@@ -942,6 +942,17 @@ require('lazy').setup({
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
   },
+  {
+    'tribela/transparent.nvim',
+    event = 'VimEnter',
+    config = true,
+    opts = {
+        auto = true, -- Automatically applies transparent
+        extra_groups = {}, -- If you want to add some groups to be transparent. eg: { 'Pmenu', 'CocFloating' }
+        excludes = {}, -- If you want to excludes from default transparent groups. eg: { 'LineNr' }
+    },
+  },
+
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
