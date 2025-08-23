2c5b15a3d39c4fdf4d5f5a1e08b9b95e5c0fc1b8
sektant-gabe <65908248+sektant-gabe@users.noreply.github.com>
2025-08-23 15:35:14 -0300

    1.1.3

--- init.lua
+++ init.lua
@@ -4,8 +4,13 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- Cursor settings
+-- vim.opt.guicursor = 'n-v-c:block'
+-- Sync working directory with current file
+vim.o.autochdir = true
+
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
@@ -16,14 +21,23 @@ vim.g.have_nerd_font = false
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
-vim.o.relativenumber = false
+vim.o.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
--- vim.o.mouse = 'a'
+vim.o.mouse = 'a'
+
+-- greatest remap ver
+vim.keymap.set('x', '<leader>p', [["_dP]])
 
 -- Don't show the mode, since it's already in the status line
-vim.o.showmode = true
-
+vim.o.showmode = false
+vim.o.copyindent = true
+vim.o.textwidth = 100
+vim.o.autochdir = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.o.cmdheight = 0
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
@@ -50,10 +64,18 @@ vim.o.updatetime = 250
 
 -- Decrease mapped sequence wait time
 vim.o.timeoutlen = 300
-
+-- Y to EOL
+vim.keymap.set('n', 'Y', 'y$', { desc = 'Yank to end of line' })
 -- Configure how new splits should be opened
 vim.o.splitright = true
 vim.o.splitbelow = true
+-- Splitting & Resizing
+--vim.keymap.set('n', 'tv', ':vsplit<CR>', { desc = 'Split window vertically' })
+--vim.keymap.set('n', 'th', ':split<CR>', { desc = 'Split window horizontally' })
+vim.keymap.set('n', '<C-Up>', ':resize +2<CR>', { desc = 'Increase window height' })
+vim.keymap.set('n', '<C-Down>', ':resize -2<CR>', { desc = 'Decrease window height' })
+vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>', { desc = 'Decrease window width' })
+vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', { desc = 'Increase window width' })
 
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
@@ -65,6 +87,9 @@ vim.o.splitbelow = true
 --   and `:help lua-options-guide`
 vim.o.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.autoread = true
+vim.opt.hidden = true
+vim.opt.backspace = 'indent,eol,start'
 
 -- Preview substitutions live, as you type!
 vim.o.inccommand = 'split'
@@ -73,8 +98,8 @@ vim.o.inccommand = 'split'
 vim.o.cursorline = false
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.o.scrolloff = 3
-
+vim.o.scrolloff = 10
+vim.opt.termguicolors = true
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
@@ -95,6 +120,22 @@ keymap('', '<leader>cc', ':CMakeClean<cr>', { desc = '[C]make Clean' })
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = '[Q]uickfix list' })
 vim.keymap.set('n', '<leader>w', ':w<CR>', { desc = '[W]rite file' })
+-------- undotree --------
+-- F5 to show the undotrree
+vim.keymap.set('n', '<F4>', ':UndotreeToggle<cr>', { silent = true, desc = 'undotree' })
+-- don't take up too much horizontal space
+vim.g.undotree_SplitWidth = 25
+-- focus the undotree when displayed
+vim.g.undotree_SetFocusWhenToggle = 1
+-- save horizontal space
+vim.g.undotree_ShortIndicators = 1
+
+vim.keymap.set('n', 'H', '^', { silent = true, desc = 'start of line' })
+vim.keymap.set('n', 'L', '$', { silent = true, desc = 'end of line' })
+-- strain reduction
+vim.keymap.set('i', 'jj', '<Esc>', { silent = true, desc = 'exit insert mode' })
+vim.keymap.set('t', 'jj', '<C-\\><C-n>', { silent = true, desc = 'exit terminal' })
+vim.keymap.set('c', 'jj', '<Esc>', { silent = true, desc = 'exit command-line' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -232,7 +273,11 @@ require('lazy').setup({
   --
   -- Then, because we use the `opts` key (recommended), the configuration runs
   -- after the plugin has been loaded as `require(MODULE).setup(opts)`.
-
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+  },
   { -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
@@ -361,6 +406,27 @@ require('lazy').setup({
       pcall(require('telescope').load_extension, 'fzf')
       pcall(require('telescope').load_extension, 'ui-select')
 
+      -- Splitting & Resizing
+      vim.keymap.set('n', '<leader>Sv', ':vsplit<CR>', { desc = 'Split window vertically' })
+      vim.keymap.set('n', '<leader>Sh', ':split<CR>', { desc = 'Split window horizontally' })
+      vim.keymap.set('n', '<C-Up>', ':resize +2<CR>', { desc = 'Increase window height' })
+      vim.keymap.set('n', '<C-Down>', ':resize -2<CR>', { desc = 'Decrease window height' })
+      vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>', { desc = 'Decrease window width' })
+      vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', { desc = 'Increase window width' })
+      -- Better indenting in visual mode
+      vim.keymap.set('v', '<', '<gv', { desc = 'Indent left and reselect' })
+      vim.keymap.set('v', '>', '>gv', { desc = 'Indent right and reselect' })
+      -- Move lines up/down
+      vim.keymap.set('n', '<A-j>', ':m .+1<CR>==', { desc = 'Move line down' })
+      vim.keymap.set('n', '<A-k>', ':m .-2<CR>==', { desc = 'Move line up' })
+      vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv", { desc = 'Move selection down' })
+      vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv", { desc = 'Move selection up' })
+
+      -- Buffer navigation
+      vim.keymap.set('n', '<leader><Tab>', ':bnext<CR>', { desc = 'Next buffer' })
+      vim.keymap.set('n', '<leader>S<Tab>', ':bprevious<CR>', { desc = 'Previous buffer' })
+      -- Better J behavior
+      vim.keymap.set('n', 'J', 'mzJ`z', { desc = 'Join lines and keep cursor position' })
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
@@ -413,6 +479,15 @@ require('lazy').setup({
     },
   },
   { 'mg979/vim-visual-multi' },
+  { 'honza/vim-snippets' },
+  { 'p00f/nvim-ts-rainbow' },
+  {
+    'Civitasv/cmake-tools.nvim',
+    opts = {
+      cmake_build_directory = 'build',
+      cmake_generate_options = { '-D', 'CMAKE_EXPORT_COMPILE_COMMANDS=1' },
+    },
+  },
   {
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
@@ -852,20 +927,14 @@ require('lazy').setup({
       -- Add color to cursor
       vim.g.doom_one_cursor_coloring = true
       -- Set :terminal colors
-      vim.g.doom_one_terminal_colors = false
+      vim.g.doom_one_terminal_colors = true
       -- Enable italic comments
       vim.g.doom_one_italic_comments = false
       -- Enable TS support
       vim.g.doom_one_enable_treesitter = true
       -- Color whole diagnostic text or only underline
       vim.g.doom_one_diagnostics_text_color = false
-      -- Enable transparent background
       vim.g.doom_one_transparent_background = true
-
-      -- Pumblend transparency
-      vim.g.doom_one_pumblend_enable = false
-      vim.g.doom_one_pumblend_transparency = 60
-
       -- Plugins integration
       vim.g.doom_one_plugin_telescope = true
       vim.g.doom_one_plugin_neogit = true
@@ -875,11 +944,15 @@ require('lazy').setup({
       vim.g.doom_one_plugin_whichkey = true
       vim.g.doom_one_plugin_vim_illuminate = true
       vim.g.doom_one_plugin_lspsaga = true
+      -- Enable transparent background
 
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'lunaperche'
+      vim.cmd.colorscheme 'doom-one'
+      --   vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+      --   vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none' })
+      --   vim.api.nvim_set_hl(0, 'EndOfBuffer', { bg = 'none' })
     end,
   },
   -- Highlight todo, notes, etc in comments
@@ -1008,8 +1081,175 @@ require('lazy').setup({
     },
   },
 })
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+-- Basic autocommands
+local augroup = vim.api.nvim_create_augroup('UserConfig', {})
+-- Set filetype-specific settings
+vim.api.nvim_create_autocmd('FileType', {
+  group = augroup,
+  pattern = { 'lua', 'python', 'c', 'cpp' },
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+  end,
+})
+vim.api.nvim_create_autocmd('FileType', {
+  group = augroup,
+  pattern = { 'javascript', 'typescript', 'json', 'html', 'css' },
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+  end,
+})
+-- Return to last edit position when opening files
+vim.api.nvim_create_autocmd('BufReadPost', {
+  group = augroup,
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
+-- Auto-close terminal when process exits
+vim.api.nvim_create_autocmd('TermClose', {
+  group = augroup,
+  callback = function()
+    if vim.v.event.status == 0 then
+      vim.api.nvim_buf_delete(0, {})
+    end
+  end,
+})
+
+-- Disable line numbers in terminal
+vim.api.nvim_create_autocmd('TermOpen', {
+  group = augroup,
+  callback = function()
+    vim.opt_local.number = false
+    vim.opt_local.relativenumber = false
+    vim.opt_local.signcolumn = 'no'
+  end,
+})
+
+-- Auto-resize splits when window is resized
+vim.api.nvim_create_autocmd('VimResized', {
+  group = augroup,
+  callback = function()
+    vim.cmd 'tabdo wincmd ='
+  end,
+})
+
+-- Create directories when saving files
+vim.api.nvim_create_autocmd('BufWritePre', {
+  group = augroup,
+  callback = function()
+    local dir = vim.fn.expand '<afile>:p:h'
+    if vim.fn.isdirectory(dir) == 0 then
+      vim.fn.mkdir(dir, 'p')
+    end
+  end,
+})
+-- Performance improvements
+vim.opt.redrawtime = 10000
+vim.opt.maxmempattern = 20000
+
+-- ============================================================================
+-- FLOATING TERMINAL
+-- ============================================================================
+
+-- terminal
+local terminal_state = {
+  buf = nil,
+  win = nil,
+  is_open = false,
+}
+
+local function FloatingTerminal()
+  -- If terminal is already open, close it (toggle behavior)
+  if terminal_state.is_open and vim.api.nvim_win_is_valid(terminal_state.win) then
+    vim.api.nvim_win_close(terminal_state.win, false)
+    terminal_state.is_open = false
+    return
+  end
+
+  -- Create buffer if it doesn't exist or is invalid
+  if not terminal_state.buf or not vim.api.nvim_buf_is_valid(terminal_state.buf) then
+    terminal_state.buf = vim.api.nvim_create_buf(false, true)
+    -- Set buffer options for better terminal experience
+    vim.api.nvim_buf_set_option(terminal_state.buf, 'bufhidden', 'hide')
+  end
+
+  -- Calculate window dimensions
+  local width = math.floor(vim.o.columns * 0.8)
+  local height = math.floor(vim.o.lines * 0.8)
+  local row = math.floor((vim.o.lines - height) / 2)
+  local col = math.floor((vim.o.columns - width) / 2)
+
+  -- Create the floating window
+  terminal_state.win = vim.api.nvim_open_win(terminal_state.buf, true, {
+    relative = 'editor',
+    width = width,
+    height = height,
+    row = row,
+    col = col,
+    style = 'minimal',
+    border = 'rounded',
+  })
+
+  -- Set transparency for the floating window
+  vim.api.nvim_win_set_option(terminal_state.win, 'winblend', 0)
+
+  -- Set transparent background for the window
+  vim.api.nvim_win_set_option(terminal_state.win, 'winhighlight', 'Normal:FloatingTermNormal,FloatBorder:FloatingTermBorder')
+
+  -- Define highlight groups for transparency
+  vim.api.nvim_set_hl(0, 'FloatingTermNormal', { bg = 'none' })
+  vim.api.nvim_set_hl(0, 'FloatingTermBorder', { bg = 'none' })
+
+  -- Start terminal if not already running
+  local has_terminal = false
+  local lines = vim.api.nvim_buf_get_lines(terminal_state.buf, 0, -1, false)
+  for _, line in ipairs(lines) do
+    if line ~= '' then
+      has_terminal = true
+      break
+    end
+  end
+
+  if not has_terminal then
+    vim.fn.termopen(os.getenv 'SHELL')
+  end
+
+  terminal_state.is_open = true
+  vim.cmd 'startinsert'
+
+  -- Set up auto-close on buffer leave
+  vim.api.nvim_create_autocmd('BufLeave', {
+    buffer = terminal_state.buf,
+    callback = function()
+      if terminal_state.is_open and vim.api.nvim_win_is_valid(terminal_state.win) then
+        vim.api.nvim_win_close(terminal_state.win, false)
+        terminal_state.is_open = false
+      end
+    end,
+    once = true,
+  })
+end
+
+-- Function to explicitly close the terminal
+local function CloseFloatingTerminal()
+  if terminal_state.is_open and vim.api.nvim_win_is_valid(terminal_state.win) then
+    vim.api.nvim_win_close(terminal_state.win, false)
+    terminal_state.is_open = false
+  end
+end
+
+-- Key mappings
+vim.keymap.set('n', 'tt', FloatingTerminal, { noremap = true, silent = true, desc = 'Toggle floating terminal' })
+vim.keymap.set('t', '<Esc>', function()
+  if terminal_state.is_open then
+    vim.api.nvim_win_close(terminal_state.win, false)
+    terminal_state.is_open = false
+  end
+end, { noremap = true, silent = true, desc = 'Close floating terminal from terminal mode' })
